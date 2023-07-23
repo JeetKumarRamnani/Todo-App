ef745ba0ed291f9f5eade423a6a826e5644d53c6
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-07-23 23:22:16 +0500

    edit functionality added in todo

--- script.js
+++ script.js
@@ -10,43 +10,57 @@ const todoValue = [];
 
 // Event listener for the Add Task button
 submitEl.addEventListener("click", () => {
-  // Check if the input field is empty, if so, return early
+  if (submitEl.value == "Done Editing") {
+    submitEl.value = "Submit A Todo";
+    // inpEl.value = "";
+    return;
+  }
   if (!inpEl.value) return;
 
-  // Push the value from the input field to the todoValue array
   todoValue.push(inpEl.value);
 
-  // Clear the input field after adding the task
   inpEl.value = "";
 
-  // Get the latest task value
   const value = todoValue[todoValue.length - 1];
 
-  // Log the latest task value to the console
   console.log(value);
 
-  // Create a new task element and append it to the todoContainer
   const taskElement = `
     <div class="Tasks-Container mt-2 text-sm text-white flex justify-between border-t-2 p-1 items-center">
-      <span class="font-thin flex-1 mr-2">${value}</span>
-      <button class="bg-blue-500 p-1 rounded-md cursor-pointer deleteBtn" type="button">Delete Task</button>
+      <span class="font-thin flex-1 mr-2 text-justify">${value}</span>
+      <button class="bg-green-500 p-1 rounded-md cursor-pointer editBtn" type="button">Edit Task</button>
+      <button class="bg-red-500 p-1 rounded-md cursor-pointer ml-2 deleteBtn" type="button">Delete Task</button>
     </div>
   `;
 
-  // Add the new task element to the todoContainer
   todoContainer.innerHTML += taskElement;
 
-  // Get all delete buttons inside the todoContainer
   const deleteButtons = todoContainer.querySelectorAll(".deleteBtn");
+  const editButtons = todoContainer.querySelectorAll(".editBtn");
 
   // Add click event listener to each delete button
   deleteButtons.forEach((deleteButton) => {
     deleteButton.addEventListener("click", () => {
-      // Log a message when the delete button is clicked
       console.log("Deleted Successfully");
-
-      // Remove the parent element (task container) when the delete button is clicked
       deleteButton.parentElement.remove();
+    });
+  });
+
+  editButtons.forEach((editButton) => {
+    editButton.addEventListener("click", function (e) {
+      inpEl.value = e.target.previousElementSibling.textContent;
+      let previousElement = e.target.previousElementSibling;
+      console.log(submitEl);
+      submitEl.value = "Done Editing";
+      submitEl.addEventListener("click", function () {
+        if (inpEl.value === "") return;
+
+        console.log(inpEl.value);
+        previousElement.textContent = inpEl.value;
+        if (submitEl.value == "Submit A Todo") {
+          inpEl.value = "";
+        }
+      });
     });
   });
 });
